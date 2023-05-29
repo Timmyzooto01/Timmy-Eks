4f8fdfc725d441603c41521b6429a42c149a0ee0
OLUSHOLA OLUFUNMILAYO <88409151+Timmyzooto01@users.noreply.github.com>
2023-05-29 16:29:38 +0100

    Add files via upload

---
+++ data.tf
@@ -0,0 +1,13 @@
+# get all available AZs in our region
+data "aws_availability_zones" "available_azs" {
+  state = "available"
+}
+
+# https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2009
+data "aws_eks_cluster" "default" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = module.eks.cluster_id
+}
